bc49fa9f1ac79d0384f3588a57beae07e9fc8074
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-04-04 16:39:30 +0000

    more comments.

--- lab-10/lab-10.asm
+++ lab-10/lab-10.asm
@@ -35,21 +35,39 @@ UI:
         BR(_IllegalInstruction)
 
 
-store_byte: // store byte - stb(rc, literal, ra) -- [ 010000 [31:26] RC[27:22] RA[21:16] LITERAL[16:0]]
+store_byte: // store byte - stb(rc, literal, ra) -- [010000 [31:26] RC[27:22] RA[21:16] LITERAL[16:0]]
         
         // The effective address EA is computed by adding the contents of
 	// register Ra to the sign-extended 16-bit displacement
 	// literal. The low-order 8-bits of register Rc are written into
 	// the byte location in memory specified by EA. The other bytes
 	// of the memory word remain unchanged.
+        
+        // PC <= PC+4
+        // EA <= Reg[Ra] + SEXT(literal)
+        // MDATA <= Mem[EA]
+        // if EA1:0 = 0b00 then MDATA7:0   <= Reg[Rc]7:0
+        // if EA1:0 = 0b01 then MDATA15:8  <= Reg[Rc]7:0
+        // if EA1:0 = 0b10 then MDATA23:16 <= Reg[Rc]7:0
+        // if EA1:0 = 0b11 then MDATA31:24 <= Reg[Rc]7:0
+        // Mem[EA] <= MDATA
 
+        
+        
+        extract_field(r0, 25, 21, r1)   // extract rc field from trapped instruction
+        MULC(r1, 4, r1)                 // convert to byte offset into regs array
+        LD(r1, regs, r3)                // r3 <- regs[rc]
 
+        extract_field(r0, 20, 16, r2)   // extract ra field from trapped instruction
+        MULC(r2, 4, r2)                 // convert to byte offset into regs array
+        LD(r1, regs, r4)                // r4 <- regs[rc]        
+        
         restore_all_regs(regs)
         JMP(xp)
 
 
 
-load_byte: // load_byte  --  ldb(ra, literal, rc) -- [ 010000 [31:26] RC[27:22] RA[21:16] LITERAL[16:0]]
+load_byte: // load_byte  --  ldb(ra, literal, rc) -- [010000 [31:26] RC[25:21] RA[20:16] LITERAL[15:0]] ??
 
         // The effective address EA is computed by adding the contents of
         // register Ra to the sign-extended 16-bit displacement literal.
@@ -57,9 +75,26 @@ load_byte: // load_byte  --  ldb(ra, literal, rc) -- [ 010000 [31:26] RC[27:22] 
         // the low-order 8 bits of register Rc
 	// bits 31:8 of Rc are cleared.
 
+        // PC <= PC+4
+        // EA <= Reg[Ra] + SEXT(literal)
+        // MDATA <= Mem[EA]
+        // Reg[Rc]7:0 <= if EA_{1:0} = 0b00 then MDATA7:0
+        // else if EA_{1:0} = 0b01 then MDATA15:8
+        // else if EA_{1:0} = 0b10 then MDATA23:16
+        // else if EA_{1:0} = 0b11 then MDATA31:24
+        // Reg[Rc]31:8 <= 0x000000
+
         extract_field(r0, 25, 21, r1)   // extract rc field from trapped instruction
+        MULC(r1, 4, r1)                 // convert to byte offset into regs array
+        LD(r1, regs, r3)                // r3 <- regs[rc]
         
 
+        extract_field(r0, 20, 16, r2)   // extract ra field from trapped instruction
+        MULC(r2, 4, r2)                 // convert to byte offset into regs array
+        LD(r1, regs, r4)                // r4 <- regs[rc]
+        
+        extract_field(r0, 15, 0, r5)    // extract literal  but is it sign extended?
+        
         
         restore_all_regs(regs)
         JMP(xp)
